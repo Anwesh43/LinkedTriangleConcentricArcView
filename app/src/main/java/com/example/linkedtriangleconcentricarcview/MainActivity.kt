14a0c6a3735ac8ffeaf7d8832275ba6dd6de0060
Anwesh43 <anweshthecool0@gmail.com>
2020-10-29 03:45:41 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedtriangleconcentricarcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtriangleconcentricarcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtriangleconcentricarcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.triangleconcentricarcview.TriangleConcentricArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriangleConcentricArcView.create(this)
+        fullscreen()
     }
+}
+
+fun MainActivity.fullscreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
