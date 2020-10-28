d6a13e0c22f81c0b794894f9c4c9a745170909b2
Anwesh43 <anweshthecool0@gmail.com>
2020-10-29 02:31:31 +0530

    adding constants and libraries

--- triangleconcentricarcview/src/main/java/com/example/triangleconcentricarcview/TriangleConcentricArcView.kt
+++ triangleconcentricarcview/src/main/java/com/example/triangleconcentricarcview/TriangleConcentricArcView.kt
@@ -1,2 +1,27 @@
 package com.example.triangleconcentricarcview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#673AB7",
+    "#4CAF50",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val arcs : Int = 3
+val parts : Int = arcs + 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.5f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
