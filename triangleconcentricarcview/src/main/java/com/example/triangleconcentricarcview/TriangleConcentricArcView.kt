2ca15ac21b8b90106085cdc99554046d075a8c43
Anwesh43 <anweshthecool0@gmail.com>
2020-10-29 02:42:12 +0530

    adding Canvas extension methods

--- triangleconcentricarcview/src/main/java/com/example/triangleconcentricarcview/TriangleConcentricArcView.kt
+++ triangleconcentricarcview/src/main/java/com/example/triangleconcentricarcview/TriangleConcentricArcView.kt
@@ -25,8 +25,42 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.5f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val deg : Float = 360f
+val rot : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTriangleArcLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val r : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf.divideScale(arcs + 1, parts))
+    for (j in 0..1) {
+        save()
+        rotate(rot * (1f - 2 * j))
+        drawLine(0f, 0f, r * sf.divideScale(0, parts), 0f, paint)
+        restore()
+    }
+    for (j in 1..arcs) {
+        val currRot : Float = rot * sf.divideScale(j, parts)
+        val currR : Float = j * (r / arcs)
+        save()
+        drawArc(RectF(-currR, -currR, currR, currR), -currRot, currRot * 2, false, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawTriangleArcLine(scale, w, h, paint)
+}
